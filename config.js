fa1405f6f5b6ba6f0c23e746765209206482f35b
Mike Bespalov <bespalovmike@gmail.com>
2017-05-02 14:02:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-		consumer_key: '5VQ4HtrebjEp8UzhknPDZ5949'
-	,	consumer_secret: 'H4kHrortNLRTzVcGVAnZDAzOONAQoNs4IL2f5bZvMQxWkvUniK'
-	,	access_token: '4181648555-5cNK3WUCQJsbCU9byFHTBn5FFPjokkuyjjRMxjX'
-	,	access_token_secret: 'ZUcdsjPpXUeQdykMpuKlJiQDCWZxC9y9e5T3gHcAepyUj'
+		consumer_key: '****'
+	,	consumer_secret: '****'
+	,	access_token: '****'
+	,	access_token_secret: '****'
 
 }
